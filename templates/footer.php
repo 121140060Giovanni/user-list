874fbb345657cae81ecd4c14f436b2cf02f08a36
121140060Giovanni <153990551+121140060Giovanni@users.noreply.github.com>
2023-12-17 12:38:30 +0700

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,7 @@
      <div class="container-fluid">
          <div class="row align-items-center justify-content-center dark-mode" style="height: 100px;">
              <div class="col-12 text-center">
-                 Copyright@2023
+                 Copyright@121140060
              </div>
          </div>
      </div>
